a1f7f1b1b73aafae5c498ee36001ba6532748af0
Akshay <101645835+Yahska-0818@users.noreply.github.com>
2026-03-21 22:19:45 +0530

    updated conftest to configure dist file for testing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,9 +4,17 @@ import pytest
 import networkx as nx
 import pandas as pd
 from fastapi.testclient import TestClient
-from main import app, graph, algo_service
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+dist_dir = os.path.join(os.path.dirname(__file__), "..", "dist")
+os.makedirs(os.path.join(dist_dir, "assets"), exist_ok=True)
+with open(os.path.join(dist_dir, "index.html"), "w") as f:
+    f.write("index")
+with open(os.path.join(dist_dir, "test.txt"), "w") as f:
+    f.write("test")
+
+from main import app, graph, algo_service
 
 @pytest.fixture(scope="session")
 def client():
